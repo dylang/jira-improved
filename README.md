93f246fc5cd58b6d91b3a575732bfd3d615be180
Dylan Greene <dylang@gmail.com>
2014-08-06 10:44:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Jira Improved
+
+![screen shot 2014-08-06 at 12 32 23 am](https://cloud.githubusercontent.com/assets/51505/3828451/d862c4ee-1d77-11e4-9190-b45e7ab2ba09.png)
+
+
+Improves the Agile board by showing the Epic name in the cards and hiding the Done column if you use a quick filter to hide tickets in that column.
+
+
+[Add to Chrome](https://chrome.google.com/webstore/detail/jira-improved/mdfbpeoaadkecmpingophakekbicinip)
+
+Better readme, code, features, etc some day.
+
+The custom field for Epic Title is currently hard coded. Tell me what field you use and I'll see if I can auto-detect it or make it an option.
+
+
+Might not work for all Jira installs. Not created owned or supported by Jira. 
---
+++ app/scripts/contentscript.js
@@ -0,0 +1,93 @@
+'use strict';
+
+function js (AJS, GH) {
+    if (!AJS || !GH) { return; }
+
+    // jquery
+    var $ = AJS.$;
+
+    var API_URL = '/rest/api/2/';
+
+    function fromQueryString (key) {
+        var querystring = window.location.search.substring(1).split('&');
+        var params = {}, pair, d = decodeURIComponent;
+        for (var i = querystring.length - 1; i >= 0; i--) {
+            pair = querystring[i].split('=');
+            params[d(pair[0])] = d(pair[1]);
+        }
+
+        return params[key];
+    }
+
+    function addFeatureNames () {
+        var rapidView = fromQueryString('rapidView');
+        if (!rapidView) { return; }
+
+        // if Hide Done is clicked, hide the last column.
+        var $lastColumns = $('.ghx-columns li:last-child');
+        var $lastColumnHeader = $('.ghx-column-headers li:last-child');
+        if ($lastColumns.has('div').length === 0) {
+            $lastColumnHeader.hide();
+            $lastColumns.hide();
+        } else {
+            $lastColumns.show();
+            $lastColumnHeader.show();
+        }
+
+
+        GH.WorkDataLoader.getData(601).done(function(data) {
+            var issues = data.issuesData.issues;
+            var $issues = $('.ghx-issue');
+
+            var epics = {};
+
+            issues.forEach(function(issue) {
+                if (!issue) { return; }
+                if (!issue.key) { return; }
+
+                var $issue = $issues.filter('[data-issue-key=' + issue.key + ']');
+                $issue.find('.ghx-flags').hide();
+                if (issue.epic) {
+                    epics[issue.epic] = epics[issue.epic] || issue.epic;
+                    $issue.append('<a href="/browse/' + issue.epic + '" target="_blank" data-epic="' + issue.epic + '" title="' + issue.epic + '">' +
+                        '</a>');
+                }
+            });
+
+            Object.keys(epics).forEach(function(epic) {
+                // https://ticket/rest/api/2/issue/XWEB-1323?fields=summary,customfield_13259,status
+                var CUSTOM_FIELD_EPIC_NAME = 'customfield_13259';
+                $.ajax(API_URL + 'issue/' + epic + '?fields=summary,status,' + CUSTOM_FIELD_EPIC_NAME, {
+                    dataType: 'json',
+                    type: 'GET'
+                }).then(function(data) {
+                    if (!data) { return; }
+                    if (!data.fields) { return; }
+                    if (!data.fields.status) { return; }
+                    if (!data.fields[CUSTOM_FIELD_EPIC_NAME]) { return; }
+
+                    $issues.find('[data-epic=' + epic + ']')
+                        .addClass('ghx-label-' + data.fields.status.statusCategory.id)
+                        .addClass('dylan')
+                        .html(data.fields[CUSTOM_FIELD_EPIC_NAME]);
+
+                });
+            });
+
+            // remove the silly priority we don't use
+            $('.ghx-feedback').remove();
+
+            // must re-register in case of updates
+            GH.CallbackManager.registerCallback(GH.WorkController.CALLBACK_POOL_RENDERED, 'SelectMostAppropriateIssueCallback', addFeatureNames);
+        });
+    }
+
+    // To get colors, but need quickfilter so we don't query every closed feature too
+    //https://ticket/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=1023&activeQuickFilters=5283
+
+    GH.CallbackManager.registerCallback(GH.WorkController.CALLBACK_POOL_RENDERED, 'SelectMostAppropriateIssueCallback', addFeatureNames);
+}
+
+var script = document.createElement('script');
+script.textContent = '(' + (js.toString()) + ')(window.AJS, window.GH);';
+document.body.appendChild(script);
