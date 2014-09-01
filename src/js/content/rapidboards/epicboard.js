2105facbb5973f6f3702f59390b4a0d76dbb6f59
Dylan Greene <dylang@gmail.com>
2014-09-01 00:14:29 -0400

    simplified epic view

--- src/js/content/rapidboards/epicboard.js
+++ src/js/content/rapidboards/epicboard.js
@@ -5,7 +5,6 @@ var api = require('../util/api');
 var page = require('../page');
 var $ = page.$;
 
-
 function isEpic(issue) {
     return issue && (issue.typeName === 'Feature' || issue.typeName === 'Epic');
 }
@@ -30,15 +29,32 @@ function getTickets(project, startAt) {
 
         data.issues.forEach(function(issue){
 
+
             var parentTicket = issue.fields[CUSTOMFIELDS.EPIC_PARENT];
-            var summary = issue.fields.summary;
+            //var summary = issue.fields.summary;
             var status = issue.fields.status.name;
             var color = issue.fields.status.statusCategory.colorName;
+            var $parentTicket = $('[data-issue-key=' + parentTicket + ']');
 
-            $('[data-issue-key=' + parentTicket + ']').append('<a href="/browse/' + issue.key + '" target="_blank" class="' +
-            'ji-font-smaller jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-' + color + ' jira-issue-status-lozenge-new jira-issue-status-lozenge-max-width-short' +
-            '" title="' + summary + '">' +
-                status + '</a>');
+
+            var $tag = $parentTicket.find('.traffic-light .' + color);
+            if (!$tag.length) {
+                console.log(color, status);
+            }
+            var count = ($tag.data('count') || 0) + 1;
+            $tag.data('count', count).addClass('jira-issue-status-lozenge-' + color);
+            $tag.html(count);
+
+            //$parentTicket.append(trafficLight);
+            /*
+
+            var tag = '<a href="/browse/' + issue.key + '" target="_blank" ' +
+                    'data-status="' + status + '" ' +
+                'class="' +
+                        'ji-font-smaller jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-' + color + ' jira-issue-status-lozenge-new jira-issue-status-lozenge-max-width-short' +
+                        '" title="' + summary + '">' +
+                            '1 ' + status.substring(2, 0) + '</a>';
+            */
         });
 
 
@@ -47,8 +63,6 @@ function getTickets(project, startAt) {
         } else {
             $('a.aui-lozenge').tipsy({opacity: 1});
         }
-
-
 
     });
 }
@@ -59,6 +73,13 @@ function decorate(data) {
 
     if (isEpic(issues[0])){
         var project = projectOfIssue(issues[0]);
+
+        $('.ghx-issue').append('<div class="traffic-light">' +
+            '<span class="blue-gray ji-font-smaller jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-new jira-issue-status-lozenge-max-width-short"></span>' +
+            '<span class="yellow ji-font-smaller jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-new jira-issue-status-lozenge-max-width-short"></span>' +
+            '<span class="green ji-font-smaller jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-new jira-issue-status-lozenge-max-width-short"></span>' +
+        '</div>');
+
 
         getTickets(project, 0);
     }
